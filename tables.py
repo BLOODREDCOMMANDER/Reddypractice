867fa143cea48cd8c879226392cbb130a6b5c67a
BLOODREDCOMMANDER <gsharishreddy2004@gmail.com>
2026-03-11 19:41:55 +0530

    added even tables

--- tables.py
+++ tables.py
@@ -1,3 +1,8 @@
+# Program to print the multiplication table of a given number
 a = int(input("enter a number"));
 for i in range (1,21):
   print(a,"x",i,"=",a*i);
+for j in range(1,21):
+  if j%2==0:
+    print(a,"x",j,"=",a*j);
+
