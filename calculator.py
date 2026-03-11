599d6dd42734dc38fa4872689d833d9c0fe9460e
BLOODREDCOMMANDER <gsharishreddy2004@gmail.com>
2026-03-11 17:44:19 +0530

    calculator program

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = int(input("enter a number"));
+operation = input("enter the operation you want to perform (+, -, *, /): ");
+b = int(input("enter another number"));
+if operation == "+":
+    print(a+b);
+elif operation == "-":
+    print(a-b);
+elif operation == "*":
+    print(a*b);
+elif operation == "/":
+    if b != 0:
+        print(a/b);
+    else:
+        print("Error: Division by zero is not allowed.");
