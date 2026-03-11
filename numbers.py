08d1a37c61a2d637bc68d0ea8a48fadf93fbe937
BLOODREDCOMMANDER <gsharishreddy2004@gmail.com>
2026-03-11 16:39:34 +0530

    increased range from 12 to 25

---
+++ numbers.py
@@ -0,0 +1,7 @@
+a = 1
+b= 12
+if b>a:
+    for i in range(a,b):
+        if i%5==0:
+            print(i, "yeah")
+        print(i)
---
+++ prod.py
@@ -0,0 +1,4 @@
+a = int(input("enter your Birth year"));
+b = int(input("enter current year"));
+age = b-a;
+print("your age is ",age,"years");
