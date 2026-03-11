08c3f2de43d5249d538bd0a2c2f19ea4a502a70e
BLOODREDCOMMANDER <gsharishreddy2004@gmail.com>
2026-03-11 17:56:18 +0530

    deleted tables and numbers from extra

--- numbers.py
+++
@@ -1,9 +0,0 @@
-a = 1
-b= 25
-if b>a:
-    for i in range(a,b):
-        if i%5==0:
-            print(i, "yeah")
-        print(i)
-for l in range (1,10):
-    print(l*"Hello");
--- tables.py
+++
@@ -1,3 +0,0 @@
-a = int(input("enter a number"));
-for i in range (1,21):
-  print(a,"x",i,"=",a*i);
