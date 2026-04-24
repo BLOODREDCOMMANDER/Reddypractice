a59896abdb55035098c86a6b8ad3cfa9e59f2783
BLOODREDCOMMANDER <gsharishreddy2004@gmail.com>
2026-04-24 14:07:29 +0530

    new python file added

---
+++ newpy.py
@@ -0,0 +1,6 @@
+l1=int(input("Enter the length: "));
+l2=int(input("Enter the breadth: "));
+area=l1*l2;
+print("Area of rectangle is: ",area);
+perimeter=2*(l1+l2);
+print("Perimeter of rectangle is: ",perimeter);
